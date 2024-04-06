1b36d79e360c70dcacae06f36a71ac51251fe42f
Fadhilah Dwi A <fadhilahdwi.a@gmail.com>
2024-04-06 21:08:13 +0700

    init

---
+++ src/components/Header.svelte
@@ -0,0 +1,28 @@
+<header class="flex flex-col relative z-20">
+  <div
+    class="max-w-[1400px] mx-auto w-full flex items-center justify-between px py-6"
+  >
+    <a href="">
+      <h1 class="font-semibold">
+        Swoley <span class="text-indigo-400">Moley</span>
+      </h1>
+    </a>
+    <button class="md:hidden grid place-items-center">
+      <i class="fa-solid fa-bars"></i>
+    </button>
+    <nav class="hidden md:flex items-center gap-4 lg:gap-6">
+      <a
+        href="#product"
+        class="duration-200 cursor-pointer hover:text-indigo-400">Product</a
+      >
+      <a
+        href="#reviews"
+        class="duration-200 cursor-pointer hover:text-indigo-400">Reviews</a
+      >
+      <a href="#faqs" class="duration-200 cursor-pointer hover:text-indigo-400"
+        >FAQs</a
+      >
+      <button class="specialBtn">Start free today</button>
+    </nav>
+  </div>
+</header>
